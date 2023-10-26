108417c7c0727c31f0807fd0a7cc7cecee1e7680
ysakaoglu <ysakaoglu59@gmail.com>
2023-10-26 17:10:36 +0300

    ex4

---
+++ ex4 arrays/main4.js
@@ -0,0 +1,7 @@
+//map, filter, reduce, every, find, findIndex, some, sort
+//1. map
+const numbers = [1, 2, 3]
+const squared = numbers.map((num) => num * num)
+console.log(squared) //number = [1, 4, 9]
+
+//2. filter
